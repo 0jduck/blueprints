170547bf53fbc93ad5ff6ab06d6fbcfdc887aca7
0jduck <nils.bjork2@icloud.com>
2026-05-30 00:08:59 +0200

    Working first publish

--- elements/elements.js
+++ elements/elements.js
@@ -1,7 +1,7 @@
 console.log("File elements/elements.js loaded")
 
 document.addEventListener('DOMContentLoaded', () => {
-    ['header', 'fotter'].forEach(X => {
+    ['header', 'fotter', 'sidebar'].forEach(X => {
         const el = document.getElementById(X);
         if (el) {
             console.log(`Found #${X}`);
@@ -35,3 +35,31 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// background sound
+const backgroundAudio = new Audio('/elements/storm.mp3');
+backgroundAudio.loop = true;
+
+function backgroundAudioToggle() {
+  if (backgroundAudio.paused) backgroundAudio.play();
+  else backgroundAudio.pause();
+  document.getElementById('backgroundAudioStatus').textContent = `Paused: ${backgroundAudio.paused}`;
+  console.log(`Background paused ${backgroundAudio.paused}`);
+}
+
+// click sound
+const clickAudio = new Audio('/elements/click.mp3');
+let clickAudioBool = true;
+
+function clickAudioCallback(event) {
+  if (clickAudioBool) {
+    clickAudio.currentTime = 0;
+    clickAudio.play();
+  }
+}
+function clickAudioToggle() {
+  clickAudioBool = !clickAudioBool;
+  document.getElementById('clickAudioStatus').textContent = `Click noise: ${clickAudioBool}`;
+  console.log(`Click audio ${clickAudioBool}`);
+}
+
+document.addEventListener("mousedown", clickAudioCallback);
